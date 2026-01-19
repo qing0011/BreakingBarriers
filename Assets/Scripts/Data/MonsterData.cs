3a9f919001311a84461639f0556fd0fbde9104d3
qing0011 <1084929949@qq.com>
2026-01-19 13:55:15 +0800

    修改了一个文件夹的位置和添加了怪物配置表json数据

--- Assets/Scripts/Data/MonsterData.cs
+++ Assets/Scripts/Data/MonsterData.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneData
+public class MonsterData 
 {
     public int id;
     public string res;
     public string sceneName;
-    public int reward;
-    public int timeLimit;
- 
-    public int monsterId;
-   
+    public int hp;
+    public int atk;
+    public int moveSpeed;
+    public int roundSpeed;
+    public float atkOffset;
 }
